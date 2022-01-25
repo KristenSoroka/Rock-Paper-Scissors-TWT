b446ce690b613a85f25651cc98da018d539f946f
Kristen Soroka <kristen.r.soroka@gmail.com>
2022-01-25 18:10:25 +0000

    Finished game/TWT tutorial walkthru.
    I'd like to update the game to make it best of 5 rounds, or first to reach 5 pts.  Not sure which I'll do, maybe try for both but I want to save this code first

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ Rock, Paper, Scissors Project following along with Tech with Tim video 3/5
 how i would go about breaking down the tasks
 1. computer selects one of the options
 2. player selects one
-3. compare states and determine who is the winner or if it is a draw.
+3. compare states and determine who is the winner or if it is a draw***.
 4. keep track of the wins
+
+***I added the draw conditional to my game that way the computer doesn't get a point for a matching conditional.  Not sure if it would but still thought it would be good to have.
+
+Bonus challenge:
+  set up the game to be best of 5 rounds
--- main.py
+++ main.py
@@ -1,1 +1,43 @@
+#best practice to import items at the beginning
 import random
+
+# starting the game with no wins
+user_wins = 0
+computer_wins = 0
+
+options = ['rock', 'paper', 'siccors']  #list
+
+# allow the user to input rock, paper, scissors, or 'q' to quit the game
+while True:
+  user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+  if user_input == 'q':
+    #quit()  #the whole program will quit.
+    break #break out of the while loop w/o quitting whole program
+  if user_input not in options: 
+    continue 
+    #a keyword that will allow start will start the loop over
+  random_number =random.randint(0, 2)  
+  #randint both stop and start values included 
+  # rock: 0, paper: 1, scissors: 2 
+  computer_pick = options[random_number]
+  print(f"Computer picked {computer_pick}.")
+
+  if user_input == "rock" and computer_pick == "scissors":
+    print("You won!")
+    user_wins += 1
+  elif user_input == "paper" and computer_pick == "rock":
+    print("You won!")
+    user_wins += 1
+  elif user_input == "scissors" and computer_pick == "paper":
+    print("You win!")
+    user_wins += 1
+  elif user_input == computer_pick:
+    print("Draw!")
+  else:
+    print("You lost!")
+    computer_wins += 1
+    
+
+print(f"You won {user_wins} times.")
+print(f"The computer won {computer_wins} times.")
+print("goodbye") #will print if you quit the game
